c40dfed1af0ff4078c46970867b883e842b4a17a
vastudillo <vastudillo@grupoviamatica.local>
2026-05-26 14:59:31 -0500

    condiciones if

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double temperatura = 41.0;
+string diagnostico;
+
+if (temperatura < 36.0)
+{
+    diagnostico = "Hipotermia";
+}
+else if (temperatura <= 37.5)
+{
+    diagnostico = "Normal";
+}
+else if (temperatura <= 38.5)
+{
+    diagnostico = "Fiebre leve";
+}
+else if (temperatura <= 39.9)
+{
+    diagnostico = "Fiebre alta";
+}
+else
+{
+    diagnostico = "Peligroso, busca médico";
+}
+
+Console.WriteLine($"Temperatura: {temperatura}°C");
+Console.WriteLine($"Diagnóstico: {diagnostico}");
